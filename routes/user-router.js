91d7a4f9ac1ff8d3b7d505eb0b4e3c261ec91eba
Povel Batth <povel62@gmail.com>
2021-06-08 15:22:09 -0400

    Adding pretteier and eslint fixes

--- routes/user-router.js
+++ routes/user-router.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require("express");
 
-const UserCtrl = require('../controllers/user-ctrl')
+const UserCtrl = require("../controllers/user-ctrl");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/user', UserCtrl.createUser)
-router.put('/user/:id', UserCtrl.updateUser)
-router.delete('/user/:id', UserCtrl.verifyToken, UserCtrl.deleteUser)
-router.get('/user/:id', UserCtrl.getUserById)
-router.get('/user', UserCtrl.getUserByEmail)
-router.post('/user/login', UserCtrl.getUserByEmailAndPassword)
-router.get('/users', UserCtrl.getUsers)
+router.post("/user", UserCtrl.createUser);
+router.put("/user/:id", UserCtrl.updateUser);
+router.delete("/user/:id", UserCtrl.verifyToken, UserCtrl.deleteUser);
+router.get("/user/:id", UserCtrl.getUserById);
+router.get("/user", UserCtrl.getUserByEmail);
+router.post("/user/login", UserCtrl.getUserByEmailAndPassword);
+router.get("/users", UserCtrl.getUsers);
 
-module.exports = router
+module.exports = router;
