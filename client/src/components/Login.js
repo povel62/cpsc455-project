a5e4c3fd77cdcfa0ecdc33f27d40ede93f8b3e3f
srahul14 <shandilyaholy@gmail.com>
2021-06-10 19:04:27 -0700

    login set up

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,33 +1,71 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actions from "../redux/actions/actions";
+import {useSelector, useDispatch} from 'react-redux'
 
-export default class Login extends Component {
-  render() {
-    const login_handler = async (e) => {
-      e.preventDefault();
-      const response = await fetch("/api/world", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      alert(await response.text());
-    };
+const Login = () => {
+  const [values, setValues] = useState({
+    response: "",
+    post: "",
+    responseToPost: "",
+    username: "",
+    pwd: "",
+  });
 
-    return (
-      <div className="loginPage">
-        <h1>LOGIN PAGE</h1>
-        <form>
-          <label>e-mail</label>
-          <input type="text"></input>
-          <br />
-          <label>Password</label>
-          <input type="text"></input>
-          <br />
-          <button type="submit" onClick={login_handler}>
-            Login
-          </button>
-        </form>
-      </div>
-    );
-  }
-}
+  const username = useSelector(values => values.username)
+  const currentUser = useSelector(values => values.currentUser)
+
+  const dispatch = useDispatch()
+
+  const user = {name: "Rei"}
+
+  useEffect(() => {
+    dispatch(allActions.userActions.setUser(user))
+  }, [])
+  //const [username, setUsername] = useState("")
+  //const [pwd, setPwd] = useState("")
+
+  const login_handler = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email: values.username, password: values.pwd }),
+    });
+    
+    const body = await response.text();
+    alert(body);
+    setValues({ ...values, responseToPost: body });
+  };
+
+  return (
+    <div className="loginPage">
+      <h1>LOGIN PAGE</h1>
+      <form>
+        <label>e-mail</label>
+        <input
+          type="text"
+          id="email_text"
+          onChange={(e) => setValues({ ...values, username: e.target.value })}
+        ></input>
+        <br />
+        <label>Password</label>
+        <input
+          type="password"
+          id="password_text"
+          onChange={(e) => setValues({ ...values, pwd: e.target.value })}
+        ></input>
+        <br />
+        <button type="submit" onClick={login_handler}>
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ client/src/components/ReduxStore.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ReduxStore = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ReduxStore
