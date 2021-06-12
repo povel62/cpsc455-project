3333ebbbd17d3d7122ad914bb138776278aba52a
bisht10 <bishtayush0@gmail.com>
2021-06-11 23:28:25 -0700

    faq

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../css/Navigation.css";
 import { Tabs, Tab } from "@material-ui/core";
 import Signup from "./Signup";
+import Faq from "./Faq";
+import Instructions from "./Instructions";
 import Login from "./Login";
 import Home from "./Home/index";
 
@@ -16,12 +18,16 @@ const Navigation = () => {
     <div>
       <Tabs value={selectedTab} onChange={handleChange}>
         <Tab label="Home" />
+        <Tab label="Instructions" />
+        <Tab label="FAQ" />
         <Tab label="Sign up" />
         <Tab label="Login" />
       </Tabs>
       {selectedTab === 0 && <Home />}
-      {selectedTab === 1 && <Signup />}
-      {selectedTab === 2 && <Login />}
+      {selectedTab === 1 && <Instructions />}
+      {selectedTab === 2 && <Faq />}
+      {selectedTab === 3 && <Signup />}
+      {selectedTab === 4 && <Login />}
     </div>
   );
 };
