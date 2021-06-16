08baceb022ff8db74d9807d6b145c00a3bdee692
srahul14 <shandilyaholy@gmail.com>
2021-06-16 09:57:00 -0700

    upload handling

--- client/src/components/Upload_button/Upload_button.js
+++ client/src/components/Upload_button/Upload_button.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-//import IconButton from "@material-ui/core/IconButton";
-//import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import * as XLSX from "xlsx";
+import DataTable from "react-data-table-component";
+import "./Upload_button.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,38 +18,80 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function UploadButtons() {
-  function handleFiles({ target }) {
-    /*const fileReader = new FileReader();
-  const name = target.accept.includes("image") ? "images" : "videos";
+  const [columns, setColumns] = useState([]);
+  const [data, setData] = useState([]);
 
-  fileReader.readAsDataURL(target.files[0]);
-  fileReader.onload = (e) => {
-    this.setState((prevState) => ({
-      [name]: [...prevState[name], e.target.result],
+  // process CSV data
+  const processData = (dataString) => {
+    const dataStringLines = dataString.split(/\r\n|\n/);
+    const headers = dataStringLines[0].split(
+      /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/
+    );
+
+    const list = [];
+    for (let i = 1; i < dataStringLines.length; i++) {
+      const row = dataStringLines[i].split(
+        /,(?![^"]*"(?:(?:[^"]*"){2})*[^"]*$)/
+      );
+      if (headers && row.length == headers.length) {
+        const obj = {};
+        for (let j = 0; j < headers.length; j++) {
+          let d = row[j];
+          if (d.length > 0) {
+            if (d[0] == '"') d = d.substring(1, d.length - 1);
+            if (d[d.length - 1] == '"') d = d.substring(d.length - 2, 1);
+          }
+          if (headers[j]) {
+            obj[headers[j]] = d;
+          }
+        }
+
+        // remove the blank rows
+        if (Object.values(obj).filter((x) => x).length > 0) {
+          list.push(obj);
+        }
+      }
+    }
+
+    // prepare columns list from headers
+    const columns = headers.map((c) => ({
+      name: c,
+      selector: c,
     }));
-    };*/
 
-    var reader = new FileReader();
+    setData(list);
+    setColumns(columns);
+  };
 
-    reader.readAsText(target.files[0]);
-
-    reader.onload = (e) => {
-      // Use reader.result
-      alert(e.target.result);
+  // handle file upload
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      /* Parse data */
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: "binary" });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      processData(data);
     };
-  }
+    reader.readAsBinaryString(file);
+  };
 
   const classes = useStyles();
 
   return (
     <div className={classes.root}>
       <input
-        accept=".csv"
+        accept=".csv,.xlsx,.xls"
         className={classes.input}
         id="contained-button-file"
         multiple
         type="file"
-        onChange={handleFiles}
+        onChange={handleFileUpload}
       />
       <label htmlFor="contained-button-file">
         <Button variant="contained" color="primary" component="span">
@@ -55,21 +99,7 @@ export default function UploadButtons() {
         </Button>
       </label>
 
-      {/*<input
-        accept="image/*"
-        className={classes.input}
-        id="icon-button-file"
-        type="file"
-      />
-      <label htmlFor="icon-button-file">
-        <IconButton
-          color="primary"
-          aria-label="upload picture"
-          component="span"
-        >
-          <PhotoCamera />
-        </IconButton>
-      </label>*/}
+      <DataTable pagination highlightOnHover columns={columns} data={data} />
     </div>
   );
 }
