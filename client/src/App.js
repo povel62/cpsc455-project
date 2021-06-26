8b99489c98f4bc3c2e40a295e3aa40595416c117
srahul14 <shandilyaholy@gmail.com>
2021-06-25 21:06:02 -0700

    debugging

--- client/src/App.js
+++ client/src/App.js
@@ -35,9 +35,6 @@ const theme = createMuiTheme({
     // Used by `getContrastText()` to maximize the contrast between
     // the background and the text.
     contrastThreshold: 3,
-    // Used by the functions below to shift a color's luminance by approximately
-    // two indexes within its tonal palette.
-    // E.g., shift from Red 500 to Red 300 or Red 700.
     tonalOffset: 0.2,
   },
 });
