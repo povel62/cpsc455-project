e2a524050e1070a3d05545b4490d68cafa9dad7f
Povel Batth <povel62@gmail.com>
2021-07-09 00:43:44 -0400

    Adding changes for download file

--- util/run_getPredictionFile.py
+++ util/run_getPredictionFile.py
@@ -1,5 +1,19 @@
 import sys
-from uploadFile import get_prediction_file
+from uploadFile import copy_file_back_to_local,openConnection1,openConnection2,closeConnection
 
-get_prediction_file(sys.argv[1], sys.argv[2])
 
+
+ssh1 = openConnection1()
+ssh2 = openConnection2(ssh1)
+# f = openFile(ssh2,CSV_FILES + "/" + sys.argv[1],sys.argv[2])
+
+
+copy_file_back_to_local(ssh2,sys.argv[1],sys.argv[2],sys.argv[3])
+
+
+# for line in sys.stdin:
+#     put_file(f, line)
+
+# closeFile(f)
+closeConnection(ssh2)
+closeConnection(ssh1)
