4898195fc8fbfbecd8c106162cb272bfec29ae0c
bisht10 <bishtayush0@gmail.com>
2021-07-15 03:36:22 -0700

    control dash data persists

--- client/src/components/ControlDashboard/ControlDashboard.js
+++ client/src/components/ControlDashboard/ControlDashboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
@@ -10,11 +10,12 @@ import "./ControlDashboard.css";
 import Row from "./Row.js";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import { useSelector } from "react-redux";
 import JobModal from "../JobModal/JobModal";
-// import Tooltip from "@material-ui/core/Tooltip";
-// import RefreshIcon from "@material-ui/icons/Refresh";
-// import IconButton from "@material-ui/core/IconButton";
+import { useSelector, useDispatch } from "react-redux";
+import { setJobs } from "../../redux/actions/actions";
+import Tooltip from "@material-ui/core/Tooltip";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import IconButton from "@material-ui/core/IconButton";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -46,13 +47,14 @@ const createData = (
 };
 
 export default function ControlDashboard() {
+  const dispatch = useDispatch();
+
   const axios = require("axios");
-  const [data, setData] = useState([]);
   const classes = useStyles();
 
   const login_token = useSelector((state) => state.loginReducer);
 
-  let rows = data.map((entry) =>
+  let rows = login_token.jobs.map((entry) =>
     createData(
       entry.name,
       entry.status,
@@ -76,7 +78,7 @@ export default function ControlDashboard() {
           let jobData = response.data.data;
           if (jobData) {
             console.log("success get jobs");
-            setData(jobData);
+            dispatch(setJobs(jobData));
           }
         } else {
           console.log("error");
@@ -84,9 +86,6 @@ export default function ControlDashboard() {
         }
       });
   }
-
-  console.log("data here");
-  console.log(data);
 
   useEffect(() => {
     loadJobs();
@@ -101,7 +100,7 @@ export default function ControlDashboard() {
               <h1>CONTROL DASHBOARD</h1>
             </Paper>
           </Grid>
-          {/* <Grid item xs={6} sm={3}>
+          <Grid item xs={6} sm={3}>
             <Paper className={classes.paper}>
               <Tooltip title="Refresh" aria-label="Refresh">
                 <IconButton
@@ -114,7 +113,7 @@ export default function ControlDashboard() {
                 </IconButton>
               </Tooltip>
             </Paper>
-          </Grid> */}
+          </Grid>
           <Grid item xs={6} sm={3}>
             <Paper className={classes.paper}>
               <JobModal />
