b6c31757d0cd2200167b19fdb7b8d503a77413e6
srahul14 <shandilyaholy@gmail.com>
2021-07-22 08:46:26 -0700

    share modal added config

--- client/src/components/ShareModal/ShareModal.js
+++ client/src/components/ShareModal/ShareModal.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Modal from "@material-ui/core/Modal";
-// import AddStepper from "./AddStepper";
 import { FaTimesCircle } from "react-icons/fa";
 import Tooltip from "@material-ui/core/Tooltip";
 // import AddBoxIcon from "@material-ui/icons/AddBox";
