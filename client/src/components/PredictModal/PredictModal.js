16c4a094948a8eaf54e0492d3d6ac17450341271
srahul14 <shandilyaholy@gmail.com>
2021-08-01 10:41:24 -0700

    predict download added

--- client/src/components/PredictModal/PredictModal.js
+++ client/src/components/PredictModal/PredictModal.js
@@ -161,7 +161,6 @@ export default function PredictModal({
             link.click();
             link.remove();
             window.URL.revokeObjectURL(addr);
-            // setPredictCanClose(true);
           } else {
             setModalText("Download failed");
           }
